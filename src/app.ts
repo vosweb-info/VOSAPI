a956ff1450393b639b9386a2ccc16159475e74eb
Manuel Schierenberg <mail@elanum.de>
2021-03-24 20:46:18 +0100

    feat: :sparkles: add mail-service

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,43 @@
-import express, { Request, Response } from 'express';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
-import project from '../package.json';
+import path from 'path';
+import { Nodemailer } from './config/nodemailer.config';
+import { Mail } from './routes';
 
-const { PORT = 3000 } = process.env;
-const { name, version } = project;
+const { PORT = 3000, NODE_ENV } = process.env;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send(`Welcome to ${name} | v${version}`);
+app.use('/api/v1', Mail);
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '.')));
+
+  app.get('*', (req: Request, res: Response, next: NextFunction) => {
+    res.sendFile(path.join(__dirname, '.', 'index.html'));
+    next();
+  });
+}
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  const code = error.status || error.statusCode || 500;
+  res.status(code).send({
+    message: error.message,
+  });
+  next();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+Nodemailer.verify()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
+
+export default app;
---
+++ src/routes/mail.routes.ts
@@ -0,0 +1,38 @@
+import {
+  NextFunction, Request, Response, Router,
+} from 'express';
+import { Nodemailer } from '../config/nodemailer.config';
+import { ReCaptcha } from '../middlewares';
+
+const { MAIL_ADDRESS = 'local@test.com' } = process.env;
+const router = Router();
+
+router
+  .route('/mail')
+  .post(ReCaptcha, (req: Request, res: Response, next: NextFunction) => {
+    const {
+      name, email, phone, subject, message,
+    } = req.body;
+    const contactData = {
+      from: email,
+      to: MAIL_ADDRESS,
+      subject: `VOSWEB: ${subject}`,
+      template: 'email',
+      context: {
+        name, email, phone, subject, message,
+      },
+    };
+
+    Nodemailer.sendMail(contactData)
+      .then((response: any) => {
+        const { messageId } = response;
+        res.status(200).send({
+          message: 'Email successfully sent to recipient!',
+          messageId,
+        });
+        next();
+      })
+      .catch((error) => next(error));
+  });
+
+export { router as Mail };
